9b36aa58c59a0920e8c5c47a44a52cbf3f2a608b
Yohan Dalpatadu <yohandalpatadu@Yohans-MacBook-Pro.local>
2020-03-20 12:11:06 +0530

    Updated site title

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -11,7 +11,7 @@ const layoutStyle = {
 const Layout = props => (
   <div>
     <Head>
-      <title>COVID-19 Situation Report - Sri Lanka</title>
+      <title>COVID-19 Situation Report</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous"/>
       <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossOrigin="anonymous"></script>
