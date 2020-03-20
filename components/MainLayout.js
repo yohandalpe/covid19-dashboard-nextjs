9a9e3b0f6132b837967977438bb30fcd8a51add5
Yohan Dalpatadu <yohandalpatadu@Yohans-MacBook-Pro.local>
2020-03-20 22:04:32 +0530

    Changed GA Tag ID

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -17,13 +17,13 @@ const Layout = props => (
       <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossOrigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossOrigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossOrigin="anonymous"></script>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-MKRT6KQS18"></script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-LRJS5H4YVR"></script>
       <script dangerouslySetInnerHTML={{__html: `
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments)}
       gtag('js', new Date());
 
-      gtag('config', 'G-MKRT6KQS18');`}}/>
+      gtag('config', 'G-LRJS5H4YVR');`}}/>
     </Head>
     <Header />
     <Container>
